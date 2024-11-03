8a008465079d4e0a875b1415a729023617ce1e9b
Fernando-Jose-Majano-Aguirre <fernandojosemajanoaguirre@gmail.com>
2024-11-02 21:58:23 -0600

    Proyecto creado

---
+++ Guia 1/FJMA/FJMA.Temperatura.UnitTestMSTest/TemperaturaTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FJMA.Temperatura;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FJMA.Temperatura.Tests
+{
+    [TestClass()]
+    public class TemperaturaTests
+    {
+        [TestMethod()]
+        public void CelsiusAFahrenheitTest()
+        {
+            Temperatura temp = new Temperatura();
+            double result = temp.CelsiusAFahrenheit(0);
+            Assert.AreEqual(32, result);
+        }
+
+        [TestMethod()]
+        public void FahrenheitACelsiusTest()
+        {
+            Temperatura temp = new Temperatura();
+            double result = temp.FahrenheitACelsius(32);
+            Assert.AreEqual(0, result);
+        }
+    }
+}
